61c5956153eed215e1aee737297871cff30b90c2
Patrick Meade <pmeade@gmail.com>
2019-03-15 17:00:55 -0700

    Fixed unit test of placeholder func

--- test/TestHouse.cpp
+++ test/TestHouse.cpp
@@ -9,8 +9,8 @@
 TEST_CASE("F of X = x % value"){
     House house{5};
     house.value = Prime{5};
-    REQUIRE(house.func.f(4) == 4);
+    REQUIRE(house.func.f(4) == 3);
     REQUIRE(house.func.f(5) == 5);
-    REQUIRE(house.func.f(6) == 1);
-    REQUIRE(house.func.f(7) == 2);
+    REQUIRE(house.func.f(6) == 5);
+    REQUIRE(house.func.f(7) == 7);
 }
